a320fc90122ecfa8d86a25c4a858ca32d23355e1
Jennifer Jang <jennjang33@gmail.com>
2021-08-25 05:32:41 -0400

    aug 25 added subject with multiselect dropdown. replaced language/ country codes chart with multiselect dropdown.

--- src/components/add/SubmissionForm.js
+++ src/components/add/SubmissionForm.js
@@ -1,7 +1,8 @@
 import {useEffect, useState} from 'react';
 import WikiTest from './WikiTest'
 import Axios from 'axios'
-import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
+import MultiSelect from "react-multi-select-component";
+
 
 const SubmissionForm = ({toAdd,onSearch, languageSetting, formToggleOn }) => {
 
@@ -14,7 +15,7 @@ const SubmissionForm = ({toAdd,onSearch, languageSetting, formToggleOn }) => {
   const [isbn10, setIsbn10] =  useState('')
   const [isbn13, setIsbn13] =  useState('')
   const [bookHighlights, setBookHighlights] = useState('')
-  const [languageVersions, setLanguageVersions] = useState("EN")
+  const [languageVersions, setLanguageVersions] = useState([])
   const [previewLanguage, setPreviewLanguage] = useState('EN')
   const [subjectLinks, setSubjectLinks] = useState([])//use what?
   const [shelfLanguage, setShelfLanguage] = useState('EN')//use what?
@@ -23,12 +24,763 @@ const SubmissionForm = ({toAdd,onSearch, languageSetting, formToggleOn }) => {
   const [earliestPublicationYear, setEarliestPublicationYear] = useState(0)
   const [bookLength, setBookLength] = useState("")
 
-  const[languageCodeHelp, setLanguageCodeHelp] = useState(false)
   const[newShelf,setNewShelf] = useState(true)
   const [allShelves, setAllShelves]=useState(false)
   const [shelfId, setShelfId]=useState('')
   const [preventResubmitShelf, setPreventResubmitShelf] = useState(false)
 
+    const selectSubjects = [
+      { label: "Ancient and Modern Languages", value: "0 Ancient and Modern Languages"},
+      { label: "Literature", value: "1 Literature" },
+      { label: "Philosophy", value: "2 Philosophy"},
+      { label: "History and Archaeology", value: "3 History and Archaeology" },
+      { label: "Anthropology and cultural sciences", value: "4 Anthropology" },
+      { label: "Human Geography", value: "5 Human Geography" },
+      { label: "Law", value: "6 Law" },
+      { label: "Politics", value: "7 Politics" },
+      { label: "Religion", value: "8 Religion" },
+      { label: "Art", value: "9 Art" },
+      { label: "Gender Issues", value: "10 Gender Issues" },
+    ];
+
+    const selectLanguageVersions = [
+    {  label: "Afar",   value: "aa"  },
+    {
+        label: "Abkhazian",
+        value: "ab"
+    },
+    {
+        label: "Avestan",
+        value: "ae"
+    },
+    {
+        label: "Afrikaans",
+        value: "af"
+    },
+    {
+        label: "Akan",
+        value: "ak"
+    },
+    {
+        label: "Amharic",
+        value: "am"
+    },
+    {
+        label: "Aragonese",
+        value: "an"
+    },
+    {
+        label: "Arabic",
+        value: "ar"
+    },
+    {
+        label: "Assamese",
+        value: "as"
+    },
+    {
+        label: "Avaric",
+        value: "av"
+    },
+    {
+        label: "Aymara",
+        value: "ay"
+    },
+    {
+        label: "Azerbaijani",
+        value: "az"
+    },
+    {
+        label: "Bashkir",
+        value: "ba"
+    },
+    {
+        label: "Belarusian",
+        value: "be"
+    },
+    {
+        label: "Bulgarian",
+        value: "bg"
+    },
+    {
+        label: "Bihari languages",
+        value: "bh"
+    },
+    {
+        label: "Bislama",
+        value: "bi"
+    },
+    {
+        label: "Bambara",
+        value: "bm"
+    },
+    {
+        label: "Bengali",
+        value: "bn"
+    },
+    {
+        label: "Tibetan",
+        value: "bo"
+    },
+    {
+        label: "Breton",
+        value: "br"
+    },
+    {
+        label: "Bosnian",
+        value: "bs"
+    },
+    {
+        label: "Catalan; Valencian",
+        value: "ca"
+    },
+    {
+        label: "Chechen",
+        value: "ce"
+    },
+    {
+        label: "Chamorro",
+        value: "ch"
+    },
+    {
+        label: "Corsican",
+        value: "co"
+    },
+    {
+        label: "Cree",
+        value: "cr"
+    },
+    {
+        label: "Czech",
+        value: "cs"
+    },
+    {
+        label: "Church Slavic; Old Slavonic; Church Slavonic; Old Bulgarian; Old Church Slavonic",
+        value: "cu"
+    },
+    {
+        label: "Chuvash",
+        value: "cv"
+    },
+    {
+        label: "Welsh",
+        value: "cy"
+    },
+    {
+        label: "Danish",
+        value: "da"
+    },
+    {
+        label: "German",
+        value: "de"
+    },
+    {
+        label: "Divehi; Dhivehi; Maldivian",
+        value: "dv"
+    },
+    {
+        label: "Dzongkha",
+        value: "dz"
+    },
+    {
+        label: "Ewe",
+        value: "ee"
+    },
+    {
+        label: "Greek, Modern (1453-)",
+        value: "el"
+    },
+    {
+        label: "English",
+        value: "en"
+    },
+    {
+        label: "Esperanto",
+        value: "eo"
+    },
+    {
+        label: "Spanish; Castilian",
+        value: "es"
+    },
+    {
+        label: "Estonian",
+        value: "et"
+    },
+    {
+        label: "Basque",
+        value: "eu"
+    },
+    {
+        label: "Persian",
+        value: "fa"
+    },
+    {
+        label: "Fulah",
+        value: "ff"
+    },
+    {
+        label: "Finnish",
+        value: "fi"
+    },
+    {
+        label: "Fijian",
+        value: "fj"
+    },
+    {
+        label: "Faroese",
+        value: "fo"
+    },
+    {
+        label: "French",
+        value: "fr"
+    },
+    {
+        label: "Western Frisian",
+        value: "fy"
+    },
+    {
+        label: "Irish",
+        value: "ga"
+    },
+    {
+        label: "Gaelic; Scottish Gaelic",
+        value: "gd"
+    },
+    {
+        label: "Galician",
+        value: "gl"
+    },
+    {
+        label: "Guarani",
+        value: "gn"
+    },
+    {
+        label: "Gujarati",
+        value: "gu"
+    },
+    {
+        label: "Manx",
+        value: "gv"
+    },
+    {
+        label: "Hausa",
+        value: "ha"
+    },
+    {
+        label: "Hebrew",
+        value: "he"
+    },
+    {
+        label: "Hindi",
+        value: "hi"
+    },
+    {
+        label: "Hiri Motu",
+        value: "ho"
+    },
+    {
+        label: "Croatian",
+        value: "hr"
+    },
+    {
+        label: "Haitian; Haitian Creole",
+        value: "ht"
+    },
+    {
+        label: "Hungarian",
+        value: "hu"
+    },
+    {
+        label: "Armenian",
+        value: "hy"
+    },
+    {
+        label: "Herero",
+        value: "hz"
+    },
+    {
+        label: "Interlingua (International Auxiliary Language Association)",
+        value: "ia"
+    },
+    {
+        label: "Indonesian",
+        value: "id"
+    },
+    {
+        label: "Interlingue; Occidental",
+        value: "ie"
+    },
+    {
+        label: "Igbo",
+        value: "ig"
+    },
+    {
+        label: "Sichuan Yi; Nuosu",
+        value: "ii"
+    },
+    {
+        label: "Inupiaq",
+        value: "ik"
+    },
+    {
+        label: "Ido",
+        value: "io"
+    },
+    {
+        label: "Icelandic",
+        value: "is"
+    },
+    {
+        label: "Italian",
+        value: "it"
+    },
+    {
+        label: "Inuktitut",
+        value: "iu"
+    },
+    {
+        label: "Japanese",
+        value: "ja"
+    },
+    {
+        label: "Javanese",
+        value: "jv"
+    },
+    {
+        label: "Georgian",
+        value: "ka"
+    },
+    {
+        label: "Kongo",
+        value: "kg"
+    },
+    {
+        label: "Kikuyu; Gikuyu",
+        value: "ki"
+    },
+    {
+        label: "Kuanyama; Kwanyama",
+        value: "kj"
+    },
+    {
+        label: "Kazakh",
+        value: "kk"
+    },
+    {
+        label: "Kalaallisut; Greenlandic",
+        value: "kl"
+    },
+    {
+        label: "Central Khmer",
+        value: "km"
+    },
+    {
+        label: "Kannada",
+        value: "kn"
+    },
+    {
+        label: "Korean",
+        value: "ko"
+    },
+    {
+        label: "Kanuri",
+        value: "kr"
+    },
+    {
+        label: "Kashmiri",
+        value: "ks"
+    },
+    {
+        label: "Kurdish",
+        value: "ku"
+    },
+    {
+        label: "Komi",
+        value: "kv"
+    },
+    {
+        label: "Cornish",
+        value: "kw"
+    },
+    {
+        label: "Kirghiz; Kyrgyz",
+        value: "ky"
+    },
+    {
+        label: "Latin",
+        value: "la"
+    },
+    {
+        label: "Luxembourgish; Letzeburgesch",
+        value: "lb"
+    },
+    {
+        label: "Ganda",
+        value: "lg"
+    },
+    {
+        label: "Limburgan; Limburger; Limburgish",
+        value: "li"
+    },
+    {
+        label: "Lingala",
+        value: "ln"
+    },
+    {
+        label: "Lao",
+        value: "lo"
+    },
+    {
+        label: "Lithuanian",
+        value: "lt"
+    },
+    {
+        label: "Luba-Katanga",
+        value: "lu"
+    },
+    {
+        label: "Latvian",
+        value: "lv"
+    },
+    {
+        label: "Malagasy",
+        value: "mg"
+    },
+    {
+        label: "Marshallese",
+        value: "mh"
+    },
+    {
+        label: "Maori",
+        value: "mi"
+    },
+    {
+        label: "Macedonian",
+        value: "mk"
+    },
+    {
+        label: "Malayalam",
+        value: "ml"
+    },
+    {
+        label: "Mongolian",
+        value: "mn"
+    },
+    {
+        label: "Marathi",
+        value: "mr"
+    },
+    {
+        label: "Malay",
+        value: "ms"
+    },
+    {
+        label: "Maltese",
+        value: "mt"
+    },
+    {
+        label: "Burmese",
+        value: "my"
+    },
+    {
+        label: "Nauru",
+        value: "na"
+    },
+    {
+        label: "Bokmål, Norwegian; Norwegian Bokmål",
+        value: "nb"
+    },
+    {
+        label: "Ndebele, North; North Ndebele",
+        value: "nd"
+    },
+    {
+        label: "Nepali",
+        value: "ne"
+    },
+    {
+        label: "Ndonga",
+        value: "ng"
+    },
+    {
+        label: "Dutch; Flemish",
+        value: "nl"
+    },
+    {
+        label: "Norwegian Nynorsk; Nynorsk, Norwegian",
+        value: "nn"
+    },
+    {
+        label: "Norwegian",
+        value: "no"
+    },
+    {
+        label: "Ndebele, South; South Ndebele",
+        value: "nr"
+    },
+    {
+        label: "Navajo; Navaho",
+        value: "nv"
+    },
+    {
+        label: "Chichewa; Chewa; Nyanja",
+        value: "ny"
+    },
+    {
+        label: "Occitan (post 1500)",
+        value: "oc"
+    },
+    {
+        label: "Ojibwa",
+        value: "oj"
+    },
+    {
+        label: "Oromo",
+        value: "om"
+    },
+    {
+        label: "Oriya",
+        value: "or"
+    },
+    {
+        label: "Ossetian; Ossetic",
+        value: "os"
+    },
+    {
+        label: "Panjabi; Punjabi",
+        value: "pa"
+    },
+    {
+        label: "Pali",
+        value: "pi"
+    },
+    {
+        label: "Polish",
+        value: "pl"
+    },
+    {
+        label: "Pushto; Pashto",
+        value: "ps"
+    },
+    {
+        label: "Portuguese",
+        value: "pt"
+    },
+    {
+        label: "Quechua",
+        value: "qu"
+    },
+    {
+        label: "Romansh",
+        value: "rm"
+    },
+    {
+        label: "Rundi",
+        value: "rn"
+    },
+    {
+        label: "Romanian; Moldavian; Moldovan",
+        value: "ro"
+    },
+    {
+        label: "Russian",
+        value: "ru"
+    },
+    {
+        label: "Kinyarwanda",
+        value: "rw"
+    },
+    {
+        label: "Sanskrit",
+        value: "sa"
+    },
+    {
+        label: "Sardinian",
+        value: "sc"
+    },
+    {
+        label: "Sindhi",
+        value: "sd"
+    },
+    {
+        label: "Northern Sami",
+        value: "se"
+    },
+    {
+        label: "Sango",
+        value: "sg"
+    },
+    {
+        label: "Sinhala; Sinhalese",
+        value: "si"
+    },
+    {
+        label: "Slovak",
+        value: "sk"
+    },
+    {
+        label: "Slovenian",
+        value: "sl"
+    },
+    {
+        label: "Samoan",
+        value: "sm"
+    },
+    {
+        label: "Shona",
+        value: "sn"
+    },
+    {
+        label: "Somali",
+        value: "so"
+    },
+    {
+        label: "Albanian",
+        value: "sq"
+    },
+    {
+        label: "Serbian",
+        value: "sr"
+    },
+    {
+        label: "Swati",
+        value: "ss"
+    },
+    {
+        label: "Sotho, Southern",
+        value: "st"
+    },
+    {
+        label: "Sundanese",
+        value: "su"
+    },
+    {
+        label: "Swedish",
+        value: "sv"
+    },
+    {
+        label: "Swahili",
+        value: "sw"
+    },
+    {
+        label: "Tamil",
+        value: "ta"
+    },
+    {
+        label: "Telugu",
+        value: "te"
+    },
+    {
+        label: "Tajik",
+        value: "tg"
+    },
+    {
+        label: "Thai",
+        value: "th"
+    },
+    {
+        label: "Tigrinya",
+        value: "ti"
+    },
+    {
+        label: "Turkmen",
+        value: "tk"
+    },
+    {
+        label: "Tagalog",
+        value: "tl"
+    },
+    {
+        label: "Tswana",
+        value: "tn"
+    },
+    {
+        label: "Tonga (Tonga Islands)",
+        value: "to"
+    },
+    {
+        label: "Turkish",
+        value: "tr"
+    },
+    {
+        label: "Tsonga",
+        value: "ts"
+    },
+    {
+        label: "Tatar",
+        value: "tt"
+    },
+    {
+        label: "Twi",
+        value: "tw"
+    },
+    {
+        label: "Tahitian",
+        value: "ty"
+    },
+    {
+        label: "Uighur; Uyghur",
+        value: "ug"
+    },
+    {
+        label: "Ukrainian",
+        value: "uk"
+    },
+    {
+        label: "Urdu",
+        value: "ur"
+    },
+    {
+        label: "Uzbek",
+        value: "uz"
+    },
+    {
+        label: "Venda",
+        value: "ve"
+    },
+    {
+        label: "Vietnamese",
+        value: "vi"
+    },
+    {
+        label: "Volapük",
+        value: "vo"
+    },
+    {
+        label: "Walloon",
+        value: "wa"
+    },
+    {
+        label: "Wolof",
+        value: "wo"
+    },
+    {
+        label: "Xhosa",
+        value: "xh"
+    },
+    {
+        label: "Yiddish",
+        value: "yi"
+    },
+    {
+        label: "Yoruba",
+        value: "yo"
+    },
+    {
+        label: "Zhuang; Chuang",
+        value: "za"
+    },
+    {
+        label: "Chinese",
+        value: "zh"
+    },
+    {
+        label: "Zulu",
+        value: "zu"
+    },
+];
+
+
+    const [subjects, setSubjects] = useState([]);
 
 
   const validateShelf = (e)=>{
@@ -51,7 +803,6 @@ const SubmissionForm = ({toAdd,onSearch, languageSetting, formToggleOn }) => {
 
 
   useEffect(()=>{
-setLanguageVersions(languageSetting)
  setPreviewLanguage(languageSetting)
 setShelfLanguage(languageSetting)
 }, [languageSetting])
@@ -153,11 +904,17 @@ setShelfLanguage(languageSetting)
        <label htmlFor="shelfDescription">Shelf Description:</label>
        <input className="form-control" type="text" id="shelfDescription" value={shelfDescription}
         onChange={(e)=>setShelfDescription(e.target.value)} placeholder="one or two short paragraphs"/>
-             <label htmlFor="languageVersions">Available in these Languages <HelpOutlineOutlinedIcon style={{cursor:"pointer"}} onClick={()=>{setLanguageCodeHelp(!languageCodeHelp)}}/>:</label>
-         <input className="form-control" type="text" id="languageVersions" value={languageVersions} onChange={(e)=>setLanguageVersions([e.target.value])} placeholder="write in language code. separate with commas"/>
+        <label htmlFor="selectSubjects">Select Covered Subjects:</label>
+  <MultiSelect
+  id="selectSubjects"
+        options={selectSubjects}
+        value={subjects}
+        onChange={setSubjects}
+        hasSelectAll={false}
+        />
         </div>
-{languageCodeHelp && <iframe src="https://datahub.io/core/language-codes/r/0.html" width="100%" height="300px" frameborder="0"></iframe>}
       <h5>Book information (partial-fill; corrections needed)</h5>
+
     <div className="form-section">
     <label htmlFor="previewLanguage">Preview Language:</label>
     <input className="form-control" type="text" id="previewLanguage" value={previewLanguage}
@@ -171,6 +928,7 @@ setShelfLanguage(languageSetting)
         <label htmlFor="earliestPublicationYear">Publication Date</label>
         <input className="form-control" type="number" id="earliestPublicationYear" value={earliestPublicationYear}
          onChange={(e)=>setEarliestPublicationYear(e.target.value)} placeholder="earliest publication year"/>
+
     </div>
            <div className="form-section readOnly">
 
@@ -187,7 +945,17 @@ setShelfLanguage(languageSetting)
                    <label htmlFor="bookHighlights">highlights</label>
                    <textarea className="form-control" type="text" id="bookHighlights" value={bookHighlights}
                     onChange={(e)=>setBookHighlights(e.target.value)} placeholder="one or two paragraphs from the book"/>
-                  </div>
+                      <label htmlFor="selectLanguageVersions">Select Language Versions:</label>
+                <MultiSelect
+                id="selectLanguageVersions"
+                      options={selectLanguageVersions}
+                      value={languageVersions}
+                      onChange={setLanguageVersions}
+                      hasSelectAll={false}
+                      />
+
+
+                 </div>
                   <input  className="btn" type="submit" style={{backgroundColor:preventResubmitShelf?"var(--inactive)":"var(--lightactionbtn)", color:preventResubmitShelf?"var(--shelfpanellistborder)":"var(--lightactionbtntext)",boxShadow:preventResubmitShelf?"none":"var(--heavyshadow)"}} onClick={(e)=>{validateShelf(e)}} value="Submit Shelf and Book"/>
 
       {toAdd && toAdd.volumeInfo.authors?toAdd.volumeInfo.authors.map(author=> <WikiTest author={author} key={author} toAdd={toAdd} earliestPublicationYear={earliestPublicationYear} setSubjectLinks={setSubjectLinks} previewLanguage={previewLanguage} subjectLinks={subjectLinks} formToggleOn={formToggleOn}/>):<WikiTest toAdd={toAdd} earliestPublicationYear={earliestPublicationYear} setSubjectLinks={setSubjectLinks} subjectLinks={subjectLinks} previewLanguage={previewLanguage} formToggleOn={formToggleOn}/>}
