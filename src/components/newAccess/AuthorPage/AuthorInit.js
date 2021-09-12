b1d5005840ac6e7585098597af38bed5b88ae5bf
Jennifer Jang <jennjang33@gmail.com>
2021-09-12 04:32:38 -0400

    sep 12 created /getauthortimeline on port 5000

--- src/components/newAccess/AuthorPage/AuthorInit.js
+++ src/components/newAccess/AuthorPage/AuthorInit.js
@@ -1,8 +1,12 @@
 import Axios from 'axios'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 const AuthorInit = ({selectedAuthor,languageSetting}) => {
 
+const [fullTimelines, setFullTimelines] = useState([{
+  details:{},
+  timelineWikiTitle:""
+}])
 
   const getYear = (date)=> {
     if(!date){return "undefined"}
@@ -17,25 +21,25 @@ const AuthorInit = ({selectedAuthor,languageSetting}) => {
   }
 
 
-
-const log2 = `450		First Slavic settlements (to 500)
-10th century
-Year	Date	Event
-910		Early stage of the Piast (Giecz-Gniezno area tribe) expansion (to 930)
-965		Merchant Abraham ben Jacob mentions the city "Karako" (Currently Kraków)
-966	April 14	Baptism of Poland
-970		Denarius becomes the currency of Poland
-972	24 July	Mieszko I defeats Odo I at the Battle of Cedynia
-989		Lesser Poland is conquered
-990		After a victory against Boleslaus II, Silesia is annexed`
-.replace(/\[\d\]/g,"")
-.replace(/Year\s+Date\s+Event/g,"")
-.replace(/\d+th century/g,"")
-.replace(/\"/g,"'")
-.replace( /(\d*)\s*(January|February|March|April|May|June|July|August|September|October|November|December)*\s*(\d*)\s*(.*)[\.\n]/g, '"$1 $3 $2":"$4",\n')
-.replace(/\"(\d+)\s+\"/g,`"$1"`)
-.replace(/\s+/," ")
-console.log(log2)
+// parser
+// const log2 = `450		First Slavic settlements (to 500)
+// 10th century
+// Year	Date	Event
+// 910		Early stage of the Piast (Giecz-Gniezno area tribe) expansion (to 930)
+// 965		Merchant Abraham ben Jacob mentions the city "Karako" (Currently Kraków)
+// 966	April 14	Baptism of Poland
+// 970		Denarius becomes the currency of Poland
+// 972	24 July	Mieszko I defeats Odo I at the Battle of Cedynia
+// 989		Lesser Poland is conquered
+// 990		After a victory against Boleslaus II, Silesia is annexed`
+// .replace(/\[\d\]/g,"")
+// .replace(/Year\s+Date\s+Event/g,"")
+// .replace(/\d+th century/g,"")
+// .replace(/\"/g,"'")
+// .replace( /(\d*)\s*(January|February|March|April|May|June|July|August|September|October|November|December)*\s*(\d*)\s*(.*)[\.\n]/g, '"$1 $3 $2":"$4",\n')
+// .replace(/\"(\d+)\s+\"/g,`"$1"`)
+// .replace(/\s+/," ")
+// console.log(log2)
 
 
 //how to extract object values
@@ -53,27 +57,58 @@ console.log(log2)
 //   }
 //   var final = finalArr.join(",")
 
+
 useEffect(()=>{
-  Axios.get("http://localhost:3001/getauthortimeline").then((res)=>{
-    console.log("data"+res.data)
-  }).then( console.log("author Timeline"))
-},[])
+  setFullTimelines([])
+  selectedAuthor.timelineLinks.forEach((timeline)=>{
+
+    const capitalize = (str) => {
+    const arr = str.split(" ")
+    const newStr = arr.map((word)=>{ return word.slice(0,1).toUpperCase() + word.slice(1,word.length)})
+    .join(" ")
+    return newStr
+    }
+
+    Axios.post("http://localhost:5000/getauthortimeline",{
+    language: languageSetting,
+     timelineWikiTitle:capitalize(timeline)
+
+    }).then((res)=>{
+      if(res.data.editions===undefined){console.log(`${timeline} not in database`); return;}else{
+        console.log(res.data)
+        setFullTimelines([...fullTimelines, res.data.editions])
+        console.log(`author Timeline ${capitalize(timeline)} retrieved`)
+      }
+
+    })
+  })
+
+},[selectedAuthor])
 
   return (
     <div>
     <h4 className="h4-details">Learn More</h4>
     <div className="Row">
+
+      <div className="Column" style={{flex:"1 1"}}>
+        <div  style={{flex:"4 4"}}><div  style={{maxHeight:"20rem", width:"auto"}}>{fullTimelines[0] &&
+
+            <div>hi</div>
+
+
+
+          }
+          </div></div>
+      <h5  style={{flex:"1 1"}} className="h5-details">Historical Background</h5>
+      <h6  style={{flex:"1 1"}} className="subtitle1-details">{selectedAuthor.timelineLinks?selectedAuthor.timelineLinks.map((timeline)=>{return timeline.slice(11)}):"Not Available for this Author"}</h6>
+      </div>
+
       <div className="Column" style={{flex:"1 1",justifyContent:"center",alignItems:"center",marginTop:"1rem"}}>
         <div  style={{flex:"4 4"}}><img  style={{maxHeight:"20rem", width:"auto"}} src={selectedAuthor.authorWikiImage}></img></div>
         <h5  style={{flex:"1 1"}} className="h5-details">{selectedAuthor.authorWikiTitle}</h5>
         <h6  style={{flex:"1 1"}} className="subtitle1-details">{`${getYear(selectedAuthor.authorBirthDate)} - ${getYear(selectedAuthor.authorDeathDate)}`}</h6>
       </div>
 
-
-      <div style={{flex:"1 1"}}>
-
-
-      </div>
 
     </div>
     </div>
